34dc6c7d76f6558014180a2a25d3ac1501710e28
四方 秀 <shu.42111227@gmail.com>
2021-08-16 20:15:33 +0900

    Initial commit

---
+++ src/Page1.jsx
@@ -0,0 +1,7 @@
+export const Page1 = () => {
+  return (
+    <div>
+      <h1>Page1ぺーじです</h1>
+    </div>
+  );
+};
---
+++ src/Page2.jsx
@@ -0,0 +1,7 @@
+export const Page2 = () => {
+  return (
+    <div>
+      <h1>Page2ぺーじです</h1>
+    </div>
+  );
+};
